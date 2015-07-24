589b60297d967c0ed100224fc255d67d079bdd2c
wq-william <415906626@qq.com>
2015-07-24 16:22:33 +0800

    增加打印信息

--- src/com/example/gittest/MainActivity.java
+++ src/com/example/gittest/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		//第一次
+		System.out.println("增加打印信息2");
 	}
 
 	@Override
